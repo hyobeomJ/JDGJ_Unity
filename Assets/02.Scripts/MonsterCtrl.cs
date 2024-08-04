fdde8c876a6ad4425dc32bfc82cc13f604ee948f
hyobeomJ <107463484+hyobeomJ@users.noreply.github.com>
2024-08-05 00:50:04 +0900

    v0.1.10.0
    챕터 10 완료

--- Assets/02.Scripts/MonsterCtrl.cs
+++ Assets/02.Scripts/MonsterCtrl.cs
@@ -51,11 +51,21 @@ public class MonsterCtrl : MonoBehaviour
         PlayerTr = GameObject.FindWithTag("PLAYER").GetComponent<Transform>();
 
         agent = GetComponent<NavMeshAgent>();
+        agent.updateRotation = false;
         anim = GetComponent<Animator>();
         bloodEffect = Resources.Load<GameObject>("GoopSprayEffect");
 
         //agent.destination = PlayerTr.position;
         
+    }
+    void update()
+    {
+        if(agent.remainingDistance >= 2.0f)
+        {
+            Vector3 direction = agent.desiredVelocity;
+            Quaternion rot = Quaternion.LookRotation(direction);
+            monsterTr.rotation = Quaternion.Slerp(monsterTr.rotation,rot,Time.deltaTime * 10.0f);
+        }
     }
 
     IEnumerator CheckMonsterState()
